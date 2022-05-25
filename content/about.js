a3546a9d7d7a0e55730392374e838d405c6107e0
Disa <disa.arista@c-aio.com>
2022-05-25 17:01:45 +0700

    add product page

---
+++ content/about.js
@@ -0,0 +1,53 @@
+const About = [
+  {
+    name: 'PURPLE ORIGINAL',
+    desc: 'Original doughnut with sweet purple sauce and pink sprinkles',
+    price: 'Rp. 10.000',
+    url: '/img/purple-basic.png'
+  },
+  {
+    name: 'PURPLE COFFEE',
+    desc: 'Hot robusta coffee drink with brown sugar and packed with cool purple cup',
+    price: 'Rp. 20.000',
+    url: '/img/purple-coffee-cup.png'
+  },
+  {
+    name: 'PURPLE SQUARE',
+    desc: 'Square shaped doughnut filled with vanilla cream',
+    price: 'Rp. 15.000',
+    url: '/img/purple-box.png'
+  },
+  {
+    name: 'PURPLE COFFEE LITER',
+    desc: 'Hot robusta coffee drink with brown sugar but liter version',
+    price: 'Rp. 100.000',
+    url: '/img/purple-coffee-liter.png'
+  },
+  {
+    name: 'PURPLE CROSS',
+    desc: 'Cross shaped doughnut filled with chocolate cream',
+    price: 'Rp. 15.000',
+    url: '/img/purple-cross.png'
+  },
+  {
+    name: 'PURPLE COFFEE POUCH',
+    desc: 'From the finest quality west java beans original premium coffee hot air roasted without flavoring essence',
+    price: 'Rp. 50.000',
+    url: '/img/purple-coffee-pouch.png'
+  },
+  {
+    name: 'PURPLE TARO PUFF',
+    desc: 'Original shaped but filled with taro cream',
+    price: 'Rp. 20.000',
+    url: '/img/purple-full.png'
+  },
+  {
+    name: 'PURPLE TRIANGLE',
+    desc: 'Triangle shaped doughnut filled with strawberry cream',
+    price: 'Rp. 15.000',
+    url: '/img/purple-triangle.png'
+  },
+  
+]
+
+export default About
